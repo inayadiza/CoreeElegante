49c43b7b69980b43ddb700008450658803cbbd5c
inayadiza <mainah.diza@gmail.com>
2024-09-10 16:54:09 +0700

    GANTI PESAN

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'Name' : 'Waode Inaya Diza Mainah',
-        'Price': 'Rp. 120.000',
-        'Description': 'White Shirt'
+        'Name' : 'Brown Cardigan',
+        'Price': 'Rp. 180.000',
+        'Description': 'A brown cardigan is a cozy, versatile layering piece, made from soft materials like wool or cotton.'
     }
 
     return render(request, "main.html", context)
