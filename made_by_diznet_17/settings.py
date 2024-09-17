417085af5546ffc4bb33ea9c8fd999c6356d3c73
inayadiza <mainah.diza@gmail.com>
2024-09-17 22:21:05 +0700

    coba push lagi

--- made_by_diznet_17/settings.py
+++ made_by_diznet_17/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ngwxmw2@u*+n@z^&n$$km$lggytzt3ux)-p&e%x$m_%i6l3up_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "waode-inaya-madebydiznet17.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "waode-inaya-coreeelegante.pbp.cs.ui.ac.id'"]
 
 
 # Application definition
