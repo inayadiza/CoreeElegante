23e34191eb327eebe6c33894c65ebce6ff9ff41f
inayadiza <mainah.diza@gmail.com>
2024-11-20 08:57:32 +0700

    tugas 9

--- made_by_diznet_17/settings.py
+++ made_by_diznet_17/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ngwxmw2@u*+n@z^&n$$km$lggytzt3ux)-p&e%x$m_%i6l3up_
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "waode-inaya-coreeelegante.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "waode-inaya-coreeelegante.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 
 # Application definition
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'made_by_diznet_17.urls'
@@ -132,3 +135,9 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.ac.id/waode.inaya/CoreeElegante", "http://pbp.cs.ui.ac.id/waode.inaya/CoreeElegante"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
